7f8520cb907a4ac81f76bcf53a97ae46608ddb95
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-25 12:26:25 +0300

    Updated the values you can get from the routing

--- app/verify/[phoneNumber].tsx
+++ app/verify/[phoneNumber].tsx
@@ -3,7 +3,10 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const VerifyOTPPage = () => {
-    const {phoneNumber} = useLocalSearchParams()
+	const { phoneNumber, signIn } = useLocalSearchParams<{
+		phoneNumber: string
+		signIn: string
+	}>()
 
 	return (
 		<View>
