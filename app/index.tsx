17f4342ccdb6100d3d0410a3bb1471abffe60513
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-25 10:42:44 +0300

    Gave the tagline a style of headline and added some styles to the headline object

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import welcomeImage from '../assets/images/welcome.png'
+import Colors from '@/constants/Colors'
 
 const welcome_image = Image.resolveAssetSource(welcomeImage).uri
 
@@ -8,7 +9,7 @@ const MainAppPage = () => {
 	return (
 		<View style={styles.container}>
 			<Image source={{ uri: welcome_image }} style={styles.image} />
-			<Text>Welcome Page</Text>
+			<Text style={styles.headline}>Welcome to Whatsapp Clone</Text>
 		</View>
 	)
 }
@@ -27,5 +28,14 @@ const styles = StyleSheet.create({
 		width: '100%',
 		height: 300,
 		marginBottom: 80
+	},
+	headline: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginVertical: 20
+	},
+	description: {},
+	link: {
+		color: Colors.gray
 	}
 })
