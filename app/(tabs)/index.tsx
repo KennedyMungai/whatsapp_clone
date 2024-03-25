2d31dbea593c9f8a3f7199ee74c106b02739b705
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-25 14:59:52 +0300

    Added the index tab page to the layout file for the tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 const TabsIndexPage = () => {
-  return (
-    <View>
-      <Text>TabsIndexPage</Text>
-    </View>
-  )
+	return (
+		<View>
+			<Text>TabsIndexPage</Text>
+		</View>
+	)
 }
 
 export default TabsIndexPage
