425cf913df968991c0346b3ca64c6935363c56ab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-25 14:44:14 +0300

    Styled the otp entry component

--- app/verify/[phoneNumber].tsx
+++ app/verify/[phoneNumber].tsx
@@ -2,6 +2,12 @@ import Colors from '@/constants/Colors'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import {
+	CodeField,
+	Cursor,
+	useBlurOnFulfill,
+	useClearByFocusCell
+} from 'react-native-confirmation-code-field'
 
 const VerifyOTPPage = () => {
 	const { phoneNumber, signIn } = useLocalSearchParams<{
@@ -19,6 +25,15 @@ const VerifyOTPPage = () => {
 		}
 	}, [code])
 
+	const CELL_COUNT = 6
+
+	const ref = useBlurOnFulfill({ value: code, cellCount: CELL_COUNT })
+
+	const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+		value: code,
+		setValue: setCode
+	})
+
 	const verifyCode = async () => {}
 
 	const verifySignIn = async () => {}
@@ -35,6 +50,32 @@ const VerifyOTPPage = () => {
 				To complete your phone number verification, please enter the 6
 				digit verification code
 			</Text>
+
+			<CodeField
+				ref={ref}
+				{...props}
+				// Use `caretHidden={false}` when users can't paste a text value, because context menu doesn't appear
+				value={code}
+				onChangeText={setCode}
+				cellCount={CELL_COUNT}
+				rootStyle={styles.codeFieldRoot}
+				keyboardType='number-pad'
+				textContentType='oneTimeCode'
+				// autoComplete={Platform.select({
+				// 	android: 'sms-otp',
+				// 	default: 'one-time-code'
+				// })}
+				testID='my-code-input'
+				renderCell={({ index, symbol, isFocused }) => (
+					<Text
+						key={index}
+						style={[styles.cellRoot, isFocused && styles.focusCell]}
+						onLayout={getCellOnLayoutHandler(index)}
+					>
+						{symbol || (isFocused ? <Cursor /> : null)}
+					</Text>
+				)}
+			/>
 
 			<TouchableOpacity style={styles.button} onPress={resendCode}>
 				<Text style={styles.buttonText}>
@@ -75,5 +116,22 @@ const styles = StyleSheet.create({
 	},
 	link: {
 		color: Colors.primary
+	},
+	codeFieldRoot: {
+		marginTop: 20
+	},
+	cellRoot: {
+		width: 50,
+		height: 50,
+		borderRadius: 10,
+		marginRight: 5,
+		lineHeight: 45,
+		fontSize: 24,
+		borderWidth: 2,
+		borderColor: '#00000030',
+		textAlign: 'center'
+	},
+	focusCell: {
+		borderColor: '#000'
 	}
 })
