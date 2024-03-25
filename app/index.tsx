eea4469e6ead4cf89752e0358504fbe5901053d6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-25 09:18:01 +0300

    Created the index file and added it to the layout file

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MainAppPage = () => {
+	return (
+		<View style={{ flex: 1, backgroundColor: 'white' }}>
+			<Text>MainAppPage</Text>
+		</View>
+	)
+}
+
+export default MainAppPage
