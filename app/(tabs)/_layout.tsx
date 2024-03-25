f3ab2497ad6a3dd03b0bc09e03dbb2770952febc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-25 14:58:52 +0300

    Added the tabs index file to the payout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,11 @@ import { Tabs } from 'expo-router'
 import React from 'react'
 
 const TabLayoutFile = () => {
-	return <Tabs></Tabs>
+	return (
+		<Tabs>
+			<Tabs.Screen name='index' />
+		</Tabs>
+	)
 }
 
 export default TabLayoutFile
