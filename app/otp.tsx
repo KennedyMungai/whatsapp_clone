288aea2627ead2813cbed926680606133ee7361e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-25 11:12:07 +0300

    Defined some consts and states

--- app/otp.tsx
+++ app/otp.tsx
@@ -1,7 +1,15 @@
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useRouter } from 'expo-router'
+import React, { useState } from 'react'
+import { KeyboardAvoidingView, Platform, StyleSheet, Text } from 'react-native'
 
 const OTPPage = () => {
+	const [loading, setLoading] = useState(false)
+	const [phoneNumber, setPhoneNumber] = useState('')
+
+	const router = useRouter()
+
+	const keyboardPlatformOffset = Platform.OS === 'ios' ? 90 : 0
+
 	return (
 		<KeyboardAvoidingView>
 			<Text>OTPPage</Text>
